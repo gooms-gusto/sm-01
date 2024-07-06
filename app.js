0f36eac98c8befa7288d0369cd500379c31d8ba2
goomsgustomac <akbar.ardiansah@outlook.co.id>
2024-07-06 19:42:03 +0700

    update vi binding

--- app.js
+++ app.js
@@ -18,17 +18,24 @@ buttonEl.addEventListener('click',addGoal);
 
 */
 
-Vue.createApp({
+const app=Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn vue',
+            courseGoalA: 'You are still rookies',
+            courseGoalB: 'You are Master Vue',
+            vueLink:'google.com'
         };
     },
-    methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-            this.enteredValue = '';
-
+methods:{
+    outputGoal(){
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5) {
+            return this.courseGoalA;
+        }else {
+            return this.courseGoalB;
         }
     }
-}).mount('#user-goal');
+}
+});
+
+app.mount('#user-goal');
